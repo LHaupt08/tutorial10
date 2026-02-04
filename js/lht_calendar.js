bf3c662863dadbe5fee843c5638e59b4be58e097
LHaupt1s <LHaupt1s@capetigers.com>
2026-02-04 10:39:00 -0600

    page 787

--- js/lht_calendar.js
+++ js/lht_calendar.js
@@ -42,6 +42,7 @@ function createCalendar(calDate) {
    var calendarHTML = "<table id='calendar_table'>";
    calendarHTML += calCaption(calDate);
    calendarHTML += calWeekdayRow();
+   calendarHTML+= calDays(calDate);
    calendarHTML += "</table>";
    return calendarHTML;
 
@@ -75,4 +76,65 @@ function calWeekdayRow() {
 
    rowHTML+="</tr>";
    return rowHTML;
+}
+
+/*Function to calculate the number of days in the month */
+function daysInMonth(calDate) {
+
+   // Array of days in each month
+   var dayCount = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+   // Extract the four digit year and month value
+   var thisYear = calDate.getFullYear();
+   var thisMonth = calDate.getMonth();
+
+   // Revise the days in Febuary for leap years
+   if (thisYear % 4 === 0) {
+      dayCount[1]=29;
+   }
+
+   // Return the number of days for the current month
+   return dayCount[thisMonth]; 
+
+}
+
+/* Function to write table rows for each day of the month*/
+function calDays(calDate) {
+
+   // Determine the starting day of the month
+
+   var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+   var weekDay = day.getDay();
+
+   // Write blank cells preceding the starting day
+
+   var htmlCode= "<tr>";
+   for (var i = 0; i < weekDay; i++) {
+      htmlCode += "<td></td>";
+   }
+
+   // Write cells for each day of the month
+
+   var totalDays = daysInMonth(calDate);
+
+   var highlightDay = calDate.getDate();
+   
+   for (var i = 1; i <=totalDays; i++) {
+      day.setDate(i);
+      weekDay = day.getDay();
+
+      if (weekDay === 0) htmlCode += "<tr>";
+
+         if (i === highlightDay) {
+            htmlCode += "<td class='calendar_dates' id='calendar_today'>" + i + "</td>";
+         } else {
+            htmlCode += "<td class='calendar_dates'>" + i + "</td>";
+         }
+
+      if (weekDay === 6) htmlCode+= "</tr>";
+
+   }
+
+   return htmlCode
+
 }
